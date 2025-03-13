4aed7a022ec5b4139bf44dc2e1d7596230c5e69f
oroshi80 <oroshi@gmail.com>
2025-03-13 11:30:07 +0000

    remove the import DialogHeader

--- app/components/PlayerDialog.tsx
+++ app/components/PlayerDialog.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
